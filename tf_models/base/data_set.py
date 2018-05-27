8586a6edb4584cb1418b3450bd33411581e53a90
spockwangs <wbbtiger@gmail.com>
2018-05-27 19:39:04 +0800

    formatted code

--- tf_models/base/data_set.py
+++ tf_models/base/data_set.py
@@ -4,5 +4,15 @@ class BaseDataSet:
     def __init__(self, options):
         self.options = options
 
-    def next_batch(self, batch_size):
+    def next_train_batch(self, batch_size):
         raise NotImplementedError
+
+    def test_set(self):
+        '''Returns test set.
+
+        Returns:
+            x
+            y 
+        '''
+        raise NotImplementedError
+        
--- tf_models/base/model.py
+++ tf_models/base/model.py
@@ -1,34 +1,32 @@
 import tensorflow as tf
 import os
+from tqdm import tqdm
+import numpy as np
 
 class BaseModel:
     def __init__(self, options):
         self.options = options
+        self.sess = tf.Session()
         # init the global step
         self.init_global_step()
-        # init the epoch counter
-        self.init_cur_epoch()
         self.init_saver()
-
+        self.build_model()
+        self.init = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
+        self.sess.run(self.init)
+        
     # save function thet save the checkpoint in the path defined in configfile
-    def save(self, sess):
+    def save(self):
         print("Saving model...")
-        self.saver.save(sess, self.options.checkpoint_dir, self.global_step_tensor)
+        self.saver.save(self.sess, self.options.checkpoint_dir, self.global_step_tensor)
         print("Model saved")
 
     # load lateset checkpoint from the experiment path defined in config_file
-    def load(self, sess):
+    def load(self):
         latest_checkpoint = tf.train.latest_checkpoint(self.options.checkpoint_dir)
         if latest_checkpoint:
             print("Loading model checkpoint {} ...\n".format(latest_checkpoint))
-            self.saver.restore(sess, latest_checkpoint)
+            self.saver.restore(self.sess, latest_checkpoint)
             print("Model loaded")
-
-    # just inialize a tensorflow variable to use it as epoch counter
-    def init_cur_epoch(self):
-        with tf.variable_scope('cur_epoch'):
-            self.cur_epoch_tensor = tf.Variable(1, trainable=False, name='cur_epoch')
-            self.increment_cur_epoch_tensor = tf.assign(self.cur_epoch_tensor, self.cur_epoch_tensor + 1)
 
     # just inialize a tensorflow variable to use it as global step counter
     def init_global_step(self):
@@ -39,5 +37,40 @@ class BaseModel:
     def init_saver(self):
         self.saver = tf.train.Saver(max_to_keep=self.options.max_to_keep)
 
+    def get_current_step(self):
+        return self.model.global_step_tensor.eval(self.sess)
+    
+    def fit(self, data_set):
+        for epoch in range(0, self.options.num_epochs, 1):
+            self.train_epoch(data_set)
+            
+    def train_epoch(self, data_set):
+        """
+        implement the logic of epoch:
+        -loop ever the number of iteration in the config and call the train step
+        -add any summaries you want using the summary
+        """
+        loop = tqdm(range(self.options.num_iter_per_epoch))
+        losses=[]
+        for it in loop:
+            x, y = next(data_set.next_batch(self.options.batch_size))
+            cur_it, loss = self.train_step(x, y)
+            losses.append(loss)
+        loss = np.mean(losses)
+        print("loss={loss}".format(loss=loss))
+        self.save()
+
     def build_model(self):
         raise NotImplementedError
+
+    def train_step(self, x, y):
+        ''' Run current train step, returns current global step and computed loss.
+        '''
+        raise NotImplementedError
+
+    def predict(self, x):
+        raise NotImplementedError
+
+    def score(self, x, y):
+        raise NotImplementedError
+    
